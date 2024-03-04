06dd77c92ccd218b83c70a2457c33f00892fdef9
baechanyong1 <chanyongbae341@gmail.com>
2024-03-04 14:18:03 +0900

    first commit

---
+++ src/config/ormconfig.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234aaaa',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      logging: true,
+    };
+  }
+}
