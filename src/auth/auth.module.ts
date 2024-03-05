773cc41dd46e4e400cbbb5150ba5f2f4de8aa96d
baechanyong1 <chanyongbae341@gmail.com>
2024-03-05 14:40:55 +0900

    docker, directory structure

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,53 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [
+    //HttpModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('ACCESS_TOKEN_EXPIRY'),
+        },
+      }),
+    }),
+    // PassportModule.register({ defaultStrategy: 'jwt' }),
+    TypeOrmModule.forFeature([
+      //   User,
+      //   AccessToken,
+      //   RefreshToken,
+      //   AccessLog,
+      //   TokenBlacklist,
+    ]),
+  ],
+  //   controllers: [AuthController],
+  providers: [
+    // UserService,
+    // AuthService,
+    // TokenBlacklistService,
+    // UserRepository,
+    // AccessTokenRepository,
+    // RefreshTokenRepository,
+    // AccessLogRepository,
+    // TokenBlacklistRepository,
+    // JwtStrategy,
+  ],
+  exports: [
+    // UserService,
+    // AuthService,
+    // TokenBlacklistService,
+    // UserRepository,
+    // AccessTokenRepository,
+    // RefreshTokenRepository,
+    // AccessLogRepository,
+    // TokenBlacklistRepository,
+    // JwtStrategy,
+  ],
 })
 export class AuthModule {}
